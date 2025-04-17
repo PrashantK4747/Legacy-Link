66329a11a94116ea5421525c7c2c97ffce54ca60
warvantepankaj <warvantepankaj@gmail.com>
2025-04-17 12:18:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,15 +24,11 @@ app.use(
 
 // PostgreSQL database connection
 const db = new pg.Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Necessary for secure connections on cloud environments
-  },
-  // user: process.env.PG_USER,
-  // host: process.env.PG_HOST,
-  // database: process.env.PG_DATABASE,
-  // password: process.env.PG_PASSWORD,
-  // port: process.env.PG_PORT || 4000,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT
 });
 
 db.connect()
