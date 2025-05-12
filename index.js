cf8bc842542e84a23b513156f20088a43f510ce9
warvantepankaj <warvantepankaj@gmail.com>
2025-05-12 16:20:47 +0530

    Updated Database

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import chatbotRouter from './public/js/chatbot.js';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware setup
 app.use(bodyParser.json());
