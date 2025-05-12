3f13f16fcd69666e3996a760d36cecfc777c4d8c
warvantepankaj <warvantepankaj@gmail.com>
2025-05-12 16:02:52 +0530

    Updated Database

--- public/js/config/gemini.js
+++ public/js/config/gemini.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import dotenv from "dotenv";
 
-const genAI = new GoogleGenerativeAI("Your Google API Key");
+const genAI = new GoogleGenerativeAI('YOUR_API_KEY');
 const model = genAI.getGenerativeModel({ 
     model: "gemini-2.0-flash",
     generationConfig: {
